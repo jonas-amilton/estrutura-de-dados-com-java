c84e90093192ba1c75ea13a730def1d402404306
jonas-amilton <jonas96@edu.unisinos.br>
2024-12-08 20:34:26 -0300

    feat: criado método toString para imprimir elementos do vetor

--- src/com/jonas/estruturadados/vetor/Vetor.java
+++ src/com/jonas/estruturadados/vetor/Vetor.java
@@ -45,4 +45,30 @@ public class Vetor {
         }
         return false;
     }
+
+    public int tamanho() {
+        return this.tamanho;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("[");
+
+        for (int i = 0; i < this.tamanho - 1; i++) {
+            sb.append(this.elementos[i]);
+            sb.append(", ");
+        }
+
+        if (this.tamanho > 0) {
+            sb.append(this.elementos[this.tamanho - 1]);
+        }
+
+        sb.append("]");
+
+        return sb.toString();
+    }
+
 }
