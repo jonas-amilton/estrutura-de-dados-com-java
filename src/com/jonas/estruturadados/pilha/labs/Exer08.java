7ea9b414f8d7f2bfbfb9e6fef669284aefcf4dce
jonas-amilton <jonas96@edu.unisinos.br>
2025-01-05 05:28:04 -0300

    fix: resolvido desafio torre de hanoi

---
+++ src/com/jonas/estruturadados/pilha/labs/Exer08.java
@@ -0,0 +1,37 @@
+package com.jonas.estruturadados.pilha.labs;
+
+import java.util.Stack;
+
+public class Exer08 {
+    public static void main(String[] args) {
+
+        Stack<Integer> original = new Stack<>();
+        Stack<Integer> destino = new Stack<>();
+        Stack<Integer> auxiliar = new Stack<>();
+
+        // Torre de Hanói
+        // comportamento de LIFO - Last In First Out
+        // primeiro a ser adicionado é o último a ser retirado
+        original.push(3);
+        original.push(2);
+        original.push(1);
+
+        torreDeHanoi(original.size(), original, destino, auxiliar);
+    }
+
+    public static void torreDeHanoi(int n, Stack<Integer> original,
+            Stack<Integer> destino, Stack<Integer> auxiliar) {
+
+        if (n > 0) {
+            torreDeHanoi(n - 1, original, auxiliar, destino);
+
+            destino.push(original.pop());
+            System.out.println("------");
+            System.out.println("Original: " + original);
+            System.out.println("Destino: " + destino);
+            System.out.println("Auxiliar: " + auxiliar);
+
+            torreDeHanoi(n - 1, auxiliar, destino, auxiliar);
+        }
+    }
+}
