dd9de146662f344677e6883c50095de679536c71
jonas-amilton <jonas96@edu.unisinos.br>
2025-01-14 22:18:13 -0300

    feat: criado classe Fila na aula 19

---
+++ src/com/jonas/estruturadados/fila/Fila.java
@@ -0,0 +1,15 @@
+package com.jonas.estruturadados.fila;
+
+import com.jonas.estruturadados.base.EstruturaEstatica;
+
+public class Fila<T> extends EstruturaEstatica<T> {
+
+    public Fila() {
+        super();
+    }
+
+    public Fila(int capacidade) {
+        super(capacidade);
+    }
+
+}
