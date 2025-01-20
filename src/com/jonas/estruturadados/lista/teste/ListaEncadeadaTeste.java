97b69825ee2098e30b5bd678ce6d53188fe01581
jonas-amilton <jonas96@edu.unisinos.br>
2025-01-19 21:14:22 -0300

    feat: adiciona classe ListaEncadeada e No com métodos básicos

---
+++ src/com/jonas/estruturadados/lista/teste/ListaEncadeadaTeste.java
@@ -0,0 +1,12 @@
+package com.jonas.estruturadados.lista.teste;
+
+import com.jonas.estruturadados.lista.ListaEncadeada;
+
+public class ListaEncadeadaTeste {
+    public static void main(String[] args) {
+        ListaEncadeada<Integer> lista = new ListaEncadeada<>();
+        lista.adiciona(1);
+
+        System.out.println(lista);
+    }
+}
