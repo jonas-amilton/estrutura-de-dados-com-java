ca1d437a65e48eb151027a16749afb1df113ba14
jonas-amilton <jonas96@edu.unisinos.br>
2025-01-22 22:16:03 -0300

    feat: adiciona métodos de busca por elemento e por posição na classe ListaEncadeada

--- src/com/jonas/estruturadados/lista/teste/ListaEncadeadaTeste.java
+++ src/com/jonas/estruturadados/lista/teste/ListaEncadeadaTeste.java
@@ -12,14 +12,27 @@ public class ListaEncadeadaTeste {
         System.out.println(lista);
 
         lista.adiciona(2);
-
         System.out.println(lista);
 
         lista.adiciona(3);
-
         System.out.println(lista);
 
-        lista.limpa();
-        System.out.println(lista);
+        // lista.limpa();
+        // System.out.println(lista);
+
+        // buscar por elemento
+        System.out.println("buscar por elemento");
+        System.out.println(lista.busca(0));
+        System.out.println(lista.busca(1));
+        System.out.println(lista.busca(2));
+        System.out.println(lista.busca(3));
+
+        // buscar por posição
+        System.out.println("buscar por posição");
+        System.out.println(lista.buscaPorPosicao(0));
+        System.out.println(lista.buscaPorPosicao(1));
+        System.out.println(lista.buscaPorPosicao(2));
+        System.out.println(lista.buscaPorPosicao(-1));
+        System.out.println(lista.buscaPorPosicao(4));
     }
 }
