4c1f8db4121f5b073eb3fee59b5a50798b0e76f1
jonas-amilton <jonas96@edu.unisinos.br>
2025-01-26 17:54:57 -0300

    feat: adiciona métodos para inserir elementos no início e em posições específicas na classe ListaEncadeada

--- src/com/jonas/estruturadados/lista/teste/ListaEncadeadaTeste.java
+++ src/com/jonas/estruturadados/lista/teste/ListaEncadeadaTeste.java
@@ -4,6 +4,38 @@ import com.jonas.estruturadados.lista.ListaEncadeada;
 
 public class ListaEncadeadaTeste {
     public static void main(String[] args) {
+        // testesIniciais();
+        // adicionaInicio();
+        adicionaPosicao();
+    }
+
+    public static void adicionaPosicao(){
+        ListaEncadeada<Integer> lista = new ListaEncadeada<>();
+
+        // lista.adiciona(1, 1);
+        lista.adiciona( 1);
+        lista.adiciona( 2);
+        // lista.adiciona(5, 0);
+        lista.adiciona(4);
+
+        lista.adiciona(0,0);
+        lista.adiciona(4,5);
+        lista.adiciona(2,3);
+
+        System.out.println(lista);
+    }        
+
+    public static void adicionaInicio(){
+        ListaEncadeada<Integer> lista = new ListaEncadeada<>();
+
+        lista.adicionaInicio(3);
+        lista.adicionaInicio(2);
+        lista.adicionaInicio(1);
+
+        System.out.println(lista);
+    }
+
+    public static void testesIniciais(){
         ListaEncadeada<Integer> lista = new ListaEncadeada<>();
 
         lista.adiciona(1);
